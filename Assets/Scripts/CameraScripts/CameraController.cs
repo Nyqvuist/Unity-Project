aab2d67fc3bef062dba907ed3ede2b767488103b
Hassan Shirazi <shiraziahassan@gmail.com>
2023-04-12 03:47:05 -0400

    added camera controller

---
+++ Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float offset;
+    [SerializeField] private float offsetSmoothing;
+    private Vector3 playerPosition;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.y);
+
+        //Not sure how i feel about this type of camera.
+
+        //if (player.transform.localScale.x > 0f)
+        //{
+        //    playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+
+        //}
+        //else
+        //{
+        //    playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        //}
+
+        //transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+}
