803cdcac9ce1ced350dbacba2cc662d5313dfe43
Cenric <eric.cen@hotmail.com>
2023-04-12 20:43:23 -0400

    add enemy scrip

--- Assets/Scripts/EnemyScripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyScripts/EnemyLogic.cs
@@ -4,6 +4,8 @@ public class EnemyLogic : MonoBehaviour
 {
 
     [SerializeField] private Animator animator;
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
 
     // Start is called before the first frame update
     void Start()
@@ -14,5 +16,11 @@ public class EnemyLogic : MonoBehaviour
     public void GetHit()
     {
         animator.SetTrigger("Hurt");
+        audioSource.PlayOneShot(PlayRandomSound());
+    }
+
+    AudioClip PlayRandomSound()
+    {
+        return audioClipArray[Random.Range(0, audioClipArray.Length)];
     }
 }
